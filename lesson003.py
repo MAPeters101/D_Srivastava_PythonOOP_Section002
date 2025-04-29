ca65a684574adf320ead2946185cd2c634cc816e
Mark Peters <mapeters101@earthlink.net>
2025-04-28 20:48:31 -0400

    Complete work on lesson003 from 9:40.

--- lesson003.py
+++ lesson003.py
@@ -1,54 +1,29 @@
 
 class Person:
-    def set_details(self):
-        self.name = 'John'
-        self.age = 20
+    def set_details(self, name, age):
+        self.name = name
+        self.age = age
 
     def display(self):
-        print('I am a person', self)
+        print('I am ', self.name)
 
     def greet(self):
-        print('Hello, how are you doing?', self)
+        if self.age < 80:
+            print('Hello, how are you doing?')
+        else:
+            print('Hello, how do you do?')
+        self.display()
 
 p1 = Person()
 p2 = Person()
 
-print(hex(id(Person)))
+p1.set_details('Bob', 20)
+p2.set_details('Ted', 90)
 
-print(type(p1))
-print(type(p2))
-print(hex(id(p1)))
-print(hex(id(p2)))
-print(p1)
-print(p2)
-print('-'*40)
-
-p1.display()
 p1.greet()
 
-p2.display()
 p2.greet()
-print('-'*40)
 
-p1.name = 'Tom'
-print(p1.name)
-#print(p2.name)
-
-print('-'*40)
-p1.set_details()
-p2.set_details()
-print(p1.name)
-print(p1.age)
-print(p2.name)
-print(p2.age)
-print()
-
-p2.name = 'Jack'
-p2.age = 30
-print(p1.name)
-print(p1.age)
-print(p2.name)
-print(p2.age)
 
 
 
